745b6a30e5cdf48bcb63c53f6a235dba0ced09f1
lilindian14 <jaime@astutesmartlocks.com>
2025-07-14 21:34:50 +1200

    Formatted config files

--- src/headunit_configs.h
+++ src/headunit_configs.h
@@ -10,16 +10,16 @@
  */
 
 #if defined(ANDROID_HEADUNIT_GENERIC)
-#define CONFIG_NAME "ANDROID_HEADUNIT_GENERIC"
+#define CONFIG_NAME                     "ANDROID_HEADUNIT_GENERIC"
 #define SWC_OUTPUT_ENABLE_SHORT_HOLD_MS 800     
 #define SWC_OUTPUT_ENABLE_LONG_HOLD_MS  4000    
-#define SWC_OUTPUT_DISABLE_MS   20              
+#define SWC_OUTPUT_DISABLE_MS           20              
 
 #elif defined(SONY_HEADUNIT_GENERIC)
-#define CONFIG_NAME "SONY_HEADUNIT_GENERIC"
+#define CONFIG_NAME                     "SONY_HEADUNIT_GENERIC"
 #define SWC_OUTPUT_ENABLE_SHORT_HOLD_MS 800     
 #define SWC_OUTPUT_ENABLE_LONG_HOLD_MS  4000    
-#define SWC_OUTPUT_DISABLE_MS   20              
+#define SWC_OUTPUT_DISABLE_MS           20              
 
 #else 
     #error "Define a Stereo in the platformio.ini file"

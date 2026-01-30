3a22765a42ceff65e69783a225c22c8e4cccb650
lilindian16 <jaimesequeira14@gmail.com>
2026-01-31 12:20:46 +1300

    Release v3.0.1

--- lib/headunit_swc/src/jvc/jvc_swc.hpp
+++ lib/headunit_swc/src/jvc/jvc_swc.hpp
@@ -2,12 +2,16 @@
 
 #include "headunit_swc.hpp"
 
+/*
+According to the protocol, all commands are sent LSB first so these enums
+reflect that
+*/
 enum JVC_SWC_Command {
-  JVC_VOLUME_UP_COMMAND   = 0x04,
-  JVC_VOLUME_DOWN_COMMAND = 0x05,
-  JVC_MUTE_COMMAND        = 0x0E,
-  JVC_NEXT_TRACK          = 0x12,
-  JVC_PREVIOUS_TRACK      = 0x13,
+  JVC_VOLUME_UP_COMMAND   = 0x84,
+  JVC_VOLUME_DOWN_COMMAND = 0x85,
+  JVC_MUTE_COMMAND        = 0x8E,
+  JVC_NEXT_TRACK          = 0x92,
+  JVC_PREVIOUS_TRACK      = 0x93,
   JVC_COMMAND_UNKNOWN     = 0xFF
 };
 
@@ -20,10 +24,13 @@ public:
   void on_button_held(void);
 
 private:
-  int     _gnd_en_pin;
-  uint8_t _previous_command = JVC_COMMAND_UNKNOWN;
+  int      _gnd_en_pin;
+  uint8_t  _previous_command           = JVC_COMMAND_UNKNOWN;
+  uint16_t _previous_message_timestamp = 0;
+  uint16_t _current_message_timestamp  = 0;
 
   void jvc_output_swc(uint8_t swc_command);
+  void write_byte_out(uint8_t output_byte);
   void jvc_binary_zero(void);
   void jvc_binary_one(void);
   void jvc_message_preamble(void);

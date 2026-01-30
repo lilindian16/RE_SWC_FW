91896dd91e93d98dc9c35d0533c36aa850bca97a
lilindian16 <jaimesequeira14@gmail.com>
2026-01-31 11:27:45 +1300

    Update: Improved UX of JVC input. Following recommended protocol from JVC datasheet (linked in source file)

--- lib/headunit_swc/src/jvc/jvc_swc.cpp
+++ lib/headunit_swc/src/jvc/jvc_swc.cpp
@@ -2,21 +2,23 @@
  * JVC steering wheel control interface
  * Refer to img/ folder for logic analyser captures showing
  * how this protocol was reverse engineered
+ *
+ * https://support.jvc.com/consumer/support/documents/RemoteCodes.pdf
  */
 
 #include "jvc_swc.hpp"
 
+#define JVC_DEVICE_ADDRESS                0x8F
 #define JVC_TICK_RESOLUTION_uS            530
-#define JVC_DATA_LENGTH_BITS              7
+#define JVC_DATA_LENGTH_BITS              8
 #define JVC_PREAMBLE_AGC_PULSE_LENGTH_MS  9
 #define JVC_PREAMBLE_LONG_PAUSE_LENGTH_MS 4
 #define JVC_MESSAGE_TRANMISSION_GAP_MS    7
-#define JVC_DATA_MAX_VALUE                0x7F // 7-bit
-#define JVC_HEADUNIT_ADDRESS              0x47
 #define JVC_REPEATED_MESSAGE_REPITITIONS  1
-#define JVC_NEW_MESSAGE_REPITITIONS       3
+#define JVC_NEW_MESSAGE_REPITITIONS       0
 #define JVC_MESSAGE_REPEAT_DELAY_MS       9
-#define JVC_NEW_MESSAGE_DELAY_MS          50
+#define JVC_MIN_SPACE_BETWEEN_WORDDS_MS   46
+#define JVC_MAX_SPACE_BETWEEN_WORDS_MS    60
 
 void JVC_SWC::init_jvc_swc(int gnd_en_pin) {
   this->_gnd_en_pin = gnd_en_pin;
@@ -43,23 +45,39 @@ void JVC_SWC::on_button_double_press(void) {
 void JVC_SWC::on_button_held(void) { this->jvc_output_swc(JVC_NEXT_TRACK); }
 
 void JVC_SWC::jvc_output_swc(uint8_t swc_command) {
-  uint8_t message_repitions = (this->_previous_command == swc_command)
-                                  ? JVC_REPEATED_MESSAGE_REPITITIONS
-                                  : JVC_NEW_MESSAGE_REPITITIONS;
-  this->_previous_command   = swc_command;
-  while (message_repitions) {
+
+  if (millis() - this->_previous_message_timestamp >= 60 ||
+      swc_command != this->_previous_command) {
+    /* Set timestamp to the start of the message */
+    this->_previous_message_timestamp = millis();
+    /*
+    The body of the message must be sent twice when it is a new command.
+    Older models require this to confirm the change of command. Send the command
+    now and wait to send the second command after
+    */
     jvc_message_preamble();
-    /* Create a temp copy so we can quickly bit-shift the value out */
-    uint8_t temp_swc_command = swc_command;
-    for (uint8_t i = 0; i < JVC_DATA_LENGTH_BITS; i++) {
-      (temp_swc_command & 1) ? jvc_binary_one() : jvc_binary_zero();
-      temp_swc_command = (temp_swc_command >> 1);
-    }
+    write_byte_out(JVC_DEVICE_ADDRESS);
+    write_byte_out(swc_command);
     jvc_message_postamble();
-    message_repitions--;
-    delay(JVC_MESSAGE_REPEAT_DELAY_MS);
   }
-  delay(JVC_NEW_MESSAGE_DELAY_MS);
+
+  while (millis() - this->_previous_message_timestamp <=
+         JVC_MIN_SPACE_BETWEEN_WORDDS_MS) {
+    delay(1);
+  }
+  /* Set timestamp to the start of the message */
+  this->_previous_message_timestamp = millis();
+  write_byte_out(JVC_DEVICE_ADDRESS);
+  write_byte_out(swc_command);
+  jvc_message_postamble();
+  this->_previous_command = swc_command;
+}
+
+void JVC_SWC::write_byte_out(uint8_t output_byte) {
+  for (uint8_t i = 0; i < JVC_DATA_LENGTH_BITS; i++) {
+    (output_byte & 1) ? jvc_binary_one() : jvc_binary_zero();
+    output_byte = (output_byte >> 1);
+  }
 }
 
 /* Write a binary zero. We are controlling an open-drain output so we must
@@ -71,8 +89,8 @@ void JVC_SWC::jvc_binary_zero(void) {
   delayMicroseconds(JVC_TICK_RESOLUTION_uS);
 }
 
-/* Write a binary one. We are controlling an open-drain output so we must invert
- * the signals */
+/* Write a binary one. We are controlling an open-drain output so we must
+ * invert the signals */
 void JVC_SWC::jvc_binary_one(void) {
   digitalWrite(this->_gnd_en_pin, HIGH);
   delayMicroseconds(JVC_TICK_RESOLUTION_uS);
@@ -85,19 +103,9 @@ void JVC_SWC::jvc_message_preamble(void) {
   delay(JVC_PREAMBLE_AGC_PULSE_LENGTH_MS);
   digitalWrite(this->_gnd_en_pin, LOW);
   delay(JVC_PREAMBLE_LONG_PAUSE_LENGTH_MS);
-  jvc_binary_one(); // Start bit
-  /* Write JVC Address */
-  jvc_binary_one();
-  jvc_binary_one();
-  jvc_binary_one();
-  jvc_binary_zero();
-  jvc_binary_zero();
-  jvc_binary_zero();
-  jvc_binary_one();
 }
 
 void JVC_SWC::jvc_message_postamble(void) {
   jvc_binary_one(); // Stop bit 1
-  jvc_binary_one(); // Stop bit 2
   delay(JVC_MESSAGE_TRANMISSION_GAP_MS);
 }
